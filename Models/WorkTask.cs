1a47cf8b93f33e0c8503557921c0a4ba43e500c4
Simon Timms <stimms@gmail.com>
2019-04-12 23:28:44 -0600

    Checkpoint - functioning webapp

---
+++ Models/WorkTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class WorkTask
+    {
+        public string Name { get; set; }
+        public List<Team> Teams { get; set; } = new List<Team>();
+    }
+
+    public class Team
+    {
+        public string Name { get; set; }
+        public bool Complete { get; set; } = false;
+        public bool NeedHelp { get; set; } = false;
+        public DateTime? CompletionTime { get; set; }
+        public DateTime? HelpTime { get; set; }
+    }
+}
--- UI/Pages/Index.cshtml.cs
+++ UI/Pages/Index.cshtml.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
 
 namespace test1.Pages
 {
     public class IndexModel : PageModel
     {
+        public WorkTask WorkTasks { get; set; }
         public void OnGet()
         {
-
+            WorkTasks = new WorkTask
+            {
+                Name = "Say hello to your neighbors",
+                Teams = new List<Team>
+                {
+                    new Team
+                    {
+                        Name = "The awesomes"
+                    },
+                    new Team
+                    {
+                        Name = "In need of help",
+                        NeedHelp = true,
+                        HelpTime = DateTime.Now
+                    },
+                    new Team
+                    {
+                        Name = "Amazingly fast",
+                        Complete = true,
+                        CompletionTime = DateTime.Now
+                    }
+                }
+            };
         }
     }
 }
